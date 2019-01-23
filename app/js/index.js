fcedde68209641361c507652cc26d29a6364abea
Nataliaiv <karamelshokoladovna@gmail.com>
2019-01-23 12:33:33 +0700

    поправила разметку, стили

--- app/js/index.js
+++ app/js/index.js
@@ -1,6 +1,7 @@
 'use strict';
+(function () {
 var ELEMENT_N = 3;
-var cardTemplate = $('#template-card').children('.page__card-box-wrapper'),
+var cardTemplate = $('#template-card').children('.card-box-wrapper'),
   cardsContainer = $('.page__cards-box'),
   renderedCards = [];
 
@@ -13,8 +14,7 @@ var numbersForDeclensionOfPortions = ['10', '40', '100'];
 var numberOfBonuses = ['', '2', '5'];
 var numbersForDeclensionOfBonuses = ['1', '2', '5'];
 var productWeights = ['0,5', '2', '5'];
-var inputNames = ['foiegras', 'fish', 'hun'];
-var inputIds = ['foiegras', 'fish', 'hun'];
+var inputNames = ['foiegras', 'fish', 'chicken'];
 
 function makeObject() {
   var objectArray = Array(ELEMENT_N);
@@ -31,7 +31,6 @@ function makeObject() {
     objectTemplate.numberForDeclensionOfBonuses = numbersForDeclensionOfBonuses[i];
     objectTemplate.weight = productWeights[i];
     objectTemplate.inputName = inputNames[i];
-    objectTemplate.inputId = inputIds[i];
     objectArray[i] = objectTemplate;
   }
   return objectArray;
@@ -40,21 +39,20 @@ function makeObject() {
 function createCard(product) {
   var card = cardTemplate.clone().appendTo($('.form'));
   cardTemplate.remove();
-  $('.page__card-box').attr("id", product.cardId);
-  $('.card__description-title').text(product.title);
-  $('.card__description-subtitle').text(product.additive);
-  $('.card__description-text--portion').prepend('<b>' + product.portion + '</b>' + declineNouns(product.numberForDeclensionOfPortions, [' порция', ' порции', ' порций']));
-  var bonus = $('.card__description-text--bonus');
+  $('.card-box').attr("id", product.cardId);
+  $('.card__title').text(product.title);
+  $('.card__subtitle').text(product.additive);
+  $('.card__text--portion').prepend('<b>' + product.portion + '</b>' + declineNouns(product.numberForDeclensionOfPortions, [' порция', ' порции', ' порций']));
+  var bonus = $('.card__text--bonus');
   bonus.text(declineNouns(product.numberForDeclensionOfBonuses, [' мышь', ' мыши', ' мышей']) + ' в подарок');
   bonus.prepend('<b>' + product.bonus + '</b>');
 
   if (product.portion === numberOfPortions[2]) {
-    $('.card__description-texts').append('<p class="card__description-text">заказчик доволен</p>');
+    $('.card__texts').append('<p class="card__text">заказчик доволен</p>');
   }
 
-  $('.card__description-weight').prepend(product.weight);
+  $('.card__weight').prepend(product.weight);
   $('input').prop('name', product.inputName);
-  $('input').prop('id', product.inputId);
   $('label').prop('for', product.inputId);
   $('.card').attr('data-disabled', 'Печалька,' + product.additive + ' закончился.');
   $('.card').attr('data-selected', product.cardHeading);
@@ -73,28 +71,31 @@ function renderCards(data) {
     $(fragment.append(card));
   });
   cardsContainer.append(fragment);
-  disableCard();
-  selectCard ();
+  disableCard('input:checkbox[name="chicken"]', '#templateId-3');
+  selectCard('input:checkbox[name="fish"]', '#templateId-2'); 
+};
+
+function disableCard(inputName, cardId) {
+  if ($(inputName).prop('disabled', true)) {
+    $(cardId).addClass('is-disabled');
+  }
+};
+
+function selectCard(inputName, cardId) {
+  if ($(inputName).prop('selected', true)) {
+    $(cardId).addClass('is-selected');
+  }
 };
 
 $(cardsContainer).on('click', 'section[id^="templateId-"]', statusClickHandler);
 
 function statusClickHandler(event) {
   event.preventDefault();
-  var purchase = $(this).children('.caption-under-card')[0],
-    cardHeading = $(this).children('.page__card-heading')[0];
   if ($(this).hasClass('is-selected')) {
     $(this).removeClass('is-selected');
     $(this).removeClass('is-selected-hover');
-    $(purchase).removeClass('visually-hidden');
-   $(cardHeading).addClass('visually-hidden');
-    
-
-   // document.querySelector('.caption-under-card').style.visibility = 'hidden';  
   } else {
     $(this).addClass('is-selected');
-
-    $(cardHeading).removeClass('visually-hidden');
   }
 };
 
@@ -103,30 +104,12 @@ $(cardsContainer).on('mouseleave', 'section[id^="templateId-"]', statusMouseleav
 function statusMouseleaveHandler(event) {
   event.preventDefault();
   if ($(this).hasClass('is-selected')) {
-  $(this).addClass('is-selected-hover'); 
+    $(this).addClass('is-selected-hover');
   }
 }
-
-function disableCard () {
-  if ($('input:checkbox[name="hun"]').prop('disabled', true)) {
-      $('#templateId-3').addClass('is-disabled');
-   }
-};
-
-function selectCard () {
-  if ($('input:checkbox[name="fish"]').prop('selected', true)) {
-    $('#templateId-2').addClass('is-selected'); 
-    //$('.caption-under-card').css('margin-top', '15px');
- } 
-};
 
 document.addEventListener('DOMContentLoaded', function (event) {
   event.preventDefault();
   renderCards(makeObject());
 });
-
-
-
-
-
-
+})();
